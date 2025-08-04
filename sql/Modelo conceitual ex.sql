612225a957d8438dc439a19e1b0b31969403af6a
Murilo Della Coletta <mucoletta11@gmail.com>
2025-08-04 16:55:39 -0300

    Add files via upload

---
+++ sql/Modelo conceitual ex.sql
@@ -0,0 +1,16 @@
+create database escola; -- Cria BCD
+
+use escola; -- Ativa Banco
+
+-- Cria Tabela
+create table Aluno (
+Id_Aluno int
+);
+
+create table Curso (
+Id_Curso int
+);
+
+create table Professor (
+Id_Professor int
+);
