dc34521438880bfe378ceb73841327956db4492f
Murilo Della Coletta <mucoletta11@gmail.com>
2025-11-17 16:54:36 -0300

    17/11

---
+++ Somativa - Oficina mecânica/CRUD/CRUD cliente/deletar.php
@@ -0,0 +1,20 @@
+<?php
+$conn = new mysqli("localhost", "root", "senaisp", "oficina");
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+
+$id = $_GET['id'];
+
+// Preparar a declaração
+$stmt = $conn->prepare("DELETE FROM cliente WHERE id_cliente = ?");
+// Vincular o parâmetro 'id' como um inteirpo (i)
+$stmt->bind_param("i", $id);
+
+// Executar e verificar
+if ($stmt->execute()) {
+    echo "Cliente deletado com sucesso.";
+} else {
+    echo "Erro ao deletar usuário: " . $stmt->error;
+}
+echo"<br><a href='/listar.php'>Voltar para a lista</a>";
---
+++ Somativa - Oficina mecânica/CRUD/CRUD cliente/insert.php
@@ -0,0 +1,23 @@
+<?php
+$nome_cliente = $_POST['nome_cliente'];
+$cpf_cliente = $_POST['cpf_cliente'];
+$telefone_cliente = $_POST['telefone_cliente'];
+
+$conn = new mysqli('localhost', 'root', 'senaisp', 'oficina');
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO cliente (nome_cliente, cpf_cliente, telefone_cliente) VALUES ('$nome_cliente', '$cpf_cliente', '$telefone_cliente')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Dados salvos com sucesso";
+    echo "<br><button><a href='cliente.html'>Voltar</a></button>";
+} else {
+    echo "Erro: " . $conn->error;
+}
+
+exit;
+$conn->close();
+?>
